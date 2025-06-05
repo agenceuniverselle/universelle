6f20c9f56ceb8cc0e50a5667efdbf39b000a7973
Agence universelle <universelle.ma@gmail.com>
2025-06-05 22:46:54 +0100

    Update cors.php

--- Laravel/config/cors.php
+++ Laravel/config/cors.php
@@ -1,15 +1,28 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'login', 'logout', 'register'],
+    'paths' => ['api/*', 'login', 'logout', 'register', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://frontappproject.netlify.app'],
+
+    'allowed_origins' => [
+        'https://frontappproject.netlify.app',
+        'https://universelle.ma',
+        'http://localhost:8080',
+        'http://localhost:8000',
+    ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => ['Authorization'],
+
     'max_age' => 0,
+
     'supports_credentials' => false,
 ];
+
 
 
 
