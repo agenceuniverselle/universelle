24879eaa7378d020b58b71bf2242383beb5d9cf0
Agence universelle <universelle.ma@gmail.com>
2025-06-06 09:08:41 +0100

    Update api.php

--- Laravel/routes/api.php
+++ Laravel/routes/api.php
@@ -33,6 +33,7 @@ use App\Http\Controllers\ProjectController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/ping', fn () => response()->json(['pong' => true]));
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
