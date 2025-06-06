833378d3e731924dca56d9f731658ead4324313c
Agence universelle <universelle.ma@gmail.com>
2025-06-06 11:57:35 +0100

    Update NosBiens.tsx

--- Frontend/src/pages/NosBiens.tsx
+++ Frontend/src/pages/NosBiens.tsx
@@ -45,7 +45,7 @@ const NosBiens = () => {
         }
 
         setLoading(true);
-        const response = await axios.get('http://localhost:8000/api/biens');
+        const response = await axios.get('https://universelle-backend-4u6cw.ondigitalocean.app/api/biens');
         const data = response.data.data || [];
         
         // Mettre à jour le cache
