dafc8f99f5ef5a173d062157509ea3a448d5c609
Agence universelle <universelle.ma@gmail.com>
2025-06-09 21:47:31 +0100

    Update BienCard.tsx

--- Frontend/src/components/properties/BienCard.tsx
+++ Frontend/src/components/properties/BienCard.tsx
@@ -18,10 +18,10 @@ interface BienCardProps {
 }
 
 const BienCard: React.FC<BienCardProps> = ({ bien }) => {
-const fileName = bien.images[0]?.split('/').pop();
-const firstImageUrl = fileName 
-  ? `https://back-qhore.ondigitalocean.app/Biens/images/${fileName}` 
+const firstImageUrl = bien.images[0]
+  ? `https://back-qhore.ondigitalocean.app/${bien.images[0]}`
   : undefined;
+
 
 
   console.log("Image URL:", firstImageUrl);
