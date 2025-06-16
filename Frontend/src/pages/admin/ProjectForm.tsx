84e17b12d1b5aa86291df9efb8f39b7097b27743
Agence universelle <universelle.ma@gmail.com>
2025-06-16 16:53:47 +0100

    Update ProjectForm.tsx

--- Frontend/src/pages/admin/ProjectForm.tsx
+++ Frontend/src/pages/admin/ProjectForm.tsx
@@ -378,14 +378,7 @@ const handleEditImage = (index: number, isNew: boolean) => {
 <DialogContent
   className="relative max-w-2xl max-h-[90vh] overflow-auto bg-white text-gray-800 dark:bg-gray-900 dark:text-white"
 >
-  {/* Close button personnalisé */}
-  <button
-    type="button"
-    onClick={handleClose}
-    className="absolute top-3 right-3 z-50 text-black dark:text-black hover:opacity-80"
-  >
-    <XCircle className="w-6 h-6" />
-  </button>    
+  
   <DialogHeader>
           <DialogTitle className="text-2xl font-bold text-center">
             {isEditing ? 'Modifier le Projet' : 'Ajouter un Nouveau Projet'}
