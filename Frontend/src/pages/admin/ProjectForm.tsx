fb41d6b558a6db3c4a57fcea302eb5fbc76cac9e
Agence universelle <universelle.ma@gmail.com>
2025-06-16 16:28:19 +0100

    Update ProjectForm.tsx

--- Frontend/src/pages/admin/ProjectForm.tsx
+++ Frontend/src/pages/admin/ProjectForm.tsx
@@ -710,7 +710,7 @@ const handleEditImage = (index: number, isNew: boolean) => {
         className="text-black border-gray-300 hover:bg-gray-100 dark:text-white dark:border-gray-600 dark:hover:bg-gray-800"
 
 >
-  <XCircle className="h-4 w-4 mr-2 text-black" /> Annuler
+  <XCircle className="h-4 w-4 mr-2 text-black dark:text-black" /> Annuler
 </Button>
 
                 <Button
